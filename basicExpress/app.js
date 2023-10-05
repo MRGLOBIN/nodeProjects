c58e5a2cc2fff3e039674c4e25dbb6935341370f
Muhammad Asim <asimqwe2@gmail.com>
2023-10-05 23:44:56 +0500

    basic express homepage

--- basicExpress/app.js
+++ basicExpress/app.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const mailer = require('nodemailer')
 const path = require('path')
+const pug = require('pug')
 
 const app = express()
 
-app.set('views', path.join(__dirname, ))
-app.set('view engine', jade)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', "pug")
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index',{
+		firstHouse: "simple Houses",
+		secondHouse: "Already Furnished"
+	})
+})
+
+app.get('/a', (req, res) => {
+	res.sendFile('./public/')
 })
 
 app.listen(3000)
